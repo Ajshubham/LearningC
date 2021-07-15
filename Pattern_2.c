67d1ce20938764e102fd943a1967e55f9ac6c430
Ajshubham <76531082+Ajshubham@users.noreply.github.com>
2021-07-15 11:42:58 +0530

    no changes

---
+++ Pattern_2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+    int a, b, c;
+    for(a = 5; a >= 1; a--)
+    {
+        for(b = 5; b > a; b--)
+        {
+            printf(" ");
+        }
+        for(c = a; c >= 1; c--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
