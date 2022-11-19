38cfe471101f6efdbec503e1e3e97194279e3d17
Shubham Shatabdi <76531082+Ajshubham@users.noreply.github.com>
2022-11-19 22:59:40 +0530

    Add files via upload

---
+++ vowels_ucase.c
@@ -0,0 +1,20 @@
+// C program to convert lowercase vowel to uppercase in string
+
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i = 0;
+    printf("Please enter a string: ");
+    scanf("%[^\n]", str);
+    while( str[i] != '\0' )
+    {
+        if(str[i]=='a' ||str[i]=='e' ||str[i]=='i' ||str[i]=='o' ||str[i]=='u')
+        {
+            str[i] = str[i] - 32;
+        }
+        i++;
+    }
+    printf("Vowel after converting into upper case = %s", str);
+    return 0;
+}
